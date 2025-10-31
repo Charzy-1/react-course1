180b994d8e740d13a9c8b7a1a1902ae9dc749e39
Charles Adikankwu <chazylites@gmail.com>
2025-10-31 06:37:58 +0100

    Added server proxy config for /api and /images

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -27,11 +27,11 @@ export function HomePage() {
 
   // Using useEffect hooke to enable us run the code once the component is created
   useEffect(() => {
-    axios.get("http://localhost:3000/api/products").then((response) => {
+    axios.get("/api/products").then((response) => {
       setProducts(response.data);
     });
 
-    axios.get("http://localhost:3000/api/cart-items").then((response) => {
+    axios.get("/api/cart-items").then((response) => {
       setCart(response.data);
     });
   }, []);
